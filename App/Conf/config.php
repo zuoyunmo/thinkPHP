bf9d28cf8185189c7c8ff35f4f0d3ae9f05f6105
LiaoGuobin <https://github.com/zuoyunmo/test>
2018-10-17 21:40:56 +0800

    update(URL_MODEL = 1)

--- App/Conf/config.php
+++ App/Conf/config.php
@@ -3,7 +3,7 @@ return array(
 	//'配置项'=>'配置值'
 	'name' => 'app_name',
 	'LOAD_EXT_CONFIG'=>'user',
-	'URL_MODEL'=>2,
+	'URL_MODEL'=>1,
 	'URL_HTML_SUFFIX'       => 'shtml|html|xml',  // URL伪静态后缀设置
 );
 
